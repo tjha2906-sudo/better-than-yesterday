3c409e99534a654c9cca250054090c8bcf303eb5
Tejaswi <tjha2906@gmail.com>
2026-09-13 10:08:12 +0530

    Fix x402 CORS headers

--- x402-service/src/server.ts
+++ x402-service/src/server.ts
@@ -80,7 +80,7 @@ app.use((req, res, next) => {
 
     res.header(
         "Access-Control-Allow-Headers",
-        "Content-Type, Authorization, PAYMENT-SIGNATURE, X-PAYMENT, Access-Control-Expose-Headers"
+        "Content-Type, Authorization, PAYMENT-SIGNATURE, X-PAYMENT"
     );
 
     res.header(
